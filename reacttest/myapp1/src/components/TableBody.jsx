9e0a39adea825da981a91b03135dc297cb58f508
ajay4ugit <ajay4ugit@gmail.com>
2024-12-14 14:35:38 +0530

    reactjs added

---
+++ reacttest/myapp1/src/components/TableBody.jsx
@@ -0,0 +1,13 @@
+function TableBody(){
+    return(
+        <div>
+            <tbody>
+                <tr>
+                    <td>AjayReddy(Master)</td>
+                    <td>Corporate Trainer</td>
+                </tr>
+            </tbody>
+        </div>
+    )
+}
+export default TableBody;
---
+++ reacttest/myapp1/src/components/Test2.jsx
@@ -0,0 +1,9 @@
+
+function Test2(){
+    return(
+        <div>
+            <h2>This is Test2 functional component</h2>
+        </div>
+    )
+}
+export default Test2;
